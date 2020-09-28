523e5b7a11cab07236016d49a63ef407c9d72c5d
Fomina Anna <annafomina01@gmail.com>
2020-09-28 21:20:07 +0300

    1 BUTTON - START IN CLASS

--- src/menu.py
+++ src/menu.py
@@ -3,30 +3,43 @@ from pygame import font
 
 ''' окно '''
 
-window = pygame.display.set_mode((1600, 900))
-pygame.display.set_caption('Welcom to the club, buddy')
-clock = pygame.time.Clock()
+
+class Menu:
+    def __init__(self):
+        self.window = pygame.display.set_mode((1600, 900))
+        pygame.display.set_caption('Welcom to the club, buddy')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def render(self):
+            self.clock.tick(60)
+            self.window.blit(pygame.image.load("background.jpg"), (0, 0))
+            start_button = pygame.draw.rect(self.window, (255, 20, 147), (150, 90, 500, 50));
+            myfont = pygame.font.SysFont('Comic Sans MS', 30)
+            text = myfont.render('Start game', False, (0, 0, 0))
+            self.window.blit(text, (300, 90))
+            pygame.display.update()
+
+    def catch_action(self):
+        x, y = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 150 <= x <= 650 and 90 <= y <= 140:
+                    self.running = False
+                    import src.main as g
+                    game = g.GameWindow()
+                    game.run()
+            if event.type == pygame.QUIT:
+                self.running = False
+                sys.exit()
+
+    def run(self):
+        self.render()
+        while self.running:
+            self.catch_action()
+
 
 pygame.init()
 pygame.font.init()
-bg = pygame.image.load("background.jpg")
-
-while True:
-    clock.tick(60)
-    window.blit(bg, (0, 0))
-    start_button = pygame.draw.rect(window, (255, 20, 147), (150, 90, 500, 50));
-    myfont = pygame.font.SysFont('Comic Sans MS', 30)
-    text = myfont.render('Start game', False, (0, 0, 0))
-    window.blit(text, (300, 90))
-    x, y = pygame.mouse.get_pos()
-
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if 150 <= x <= 650 and 90 <= y <= 140:
-                import src.main as g
-                game = g.GameWindow()
-                game.run()
-                sys.exit()
-        if event.type == pygame.QUIT:
-            sys.exit()
-    pygame.display.update()
+menu = Menu()
+menu.run()
