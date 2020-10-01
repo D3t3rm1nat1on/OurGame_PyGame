6d061001e72a065506ce4efd6afbf28afbc69b8f
Fomina Anna <annafomina01@gmail.com>
2020-10-01 18:24:44 +0300

    start of changing theme

--- src/menu.py
+++ src/menu.py
@@ -9,24 +9,34 @@ class Menu:
         pygame.display.set_caption('Welcome to the club, buddy')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.theme = 1
+        self.color = [
+            [(255, 20, 147), (176, 255, 46)],
+            [(176, 255, 46), (255, 20, 147)]]
         self.settings = Settings()
+        self.text_theme = [
+            [self.color[self.theme][0], (0, 0, 190, 40), 'Back', (0, 0), False, 1],
+            [self.color[self.theme][0], (150, 90, 500, 40), "Shine like a princess", (300, 90), False, -1, 0, True],
+            [self.color[self.theme][0], (150, 150, 500, 40), "Toxic frogs", (300, 150), False, -1, 1, True]
+        ]
         self.text = [
-            [(255, 20, 147), (150, 90, 500, 40), "Let's go!", (300, 90), False],
-            [(255, 20, 147), (150, 150, 500, 40), "Settings", (300, 150), False, 1],
-            [(255, 20, 147), (150, 210, 500, 40), "Results", (300, 210), False, 2],
-            [(255, 20, 147), (150, 270, 500, 40), "Achievements", (300, 270), False, 3],
-            [(255, 20, 147), (150, 330, 500, 40), "Check the connection", (300, 330), False, 4]]
+            [self.color[self.theme][0], (150, 90, 500, 40), "Let's go!", (300, 90), False, 140800],
+            [self.color[self.theme][0], (150, 150, 500, 40), "Settings", (300, 150), False, 1],
+            [self.color[self.theme][0], (150, 210, 500, 40), "Results", (300, 210), False, 2],
+            [self.color[self.theme][0], (150, 270, 500, 40), "Achievements", (300, 270), False, 3],
+            [self.color[self.theme][0], (150, 330, 500, 40), "Check the connection", (300, 330), False, 4]]
 
         self.text_settings = [
-            [(255, 20, 147), (150, 90, 500, 40), "Sound:" + str(self.settings.vol) + "%", (300, 90), False],
-            [(255, 20, 147), (150, 150, 500, 40), "Unlocked themes", (300, 150), False],
-            [(255, 20, 147), (150, 210, 500, 40), "Change connection", (300, 210), False],
-            [(255, 20, 147), (0, 0, 190, 40), 'To main menu', (0, 0), False],
-            [(255, 20, 147), (110, 90, 30, 40), '-', (120, 85), False],
-            [(255, 20, 147), (660, 90, 30, 40), '+', (670, 85), False]]
+            [self.color[self.theme][0], (150, 90, 500, 40), "Sound:" + str(self.settings.vol) + "%", (300, 90), False,
+             -1],
+            [self.color[self.theme][0], (150, 210, 500, 40), "Change connection", (300, 210), False, -1],
+            [self.color[self.theme][0], (150, 150, 500, 40), "Unlocked themes", (300, 150), False, 5],
+            [self.color[self.theme][0], (0, 0, 190, 40), 'To main menu', (0, 0), False, 0],
+            [self.color[self.theme][0], (110, 90, 30, 40), '-', (120, 85), False, -1],
+            [self.color[self.theme][0], (660, 90, 30, 40), '+', (670, 85), False, -1]]
 
         self.num_ren = 0
-        self.renders = [self.render0, self.render1]
+        self.renders = [self.render0, self.render1, 2, 3, 4, self.render5]
 
     def button(self, color_rect, coordinates, text, start_text):
         pygame.draw.rect(self.window, color_rect, coordinates)
@@ -36,8 +46,9 @@ class Menu:
     def render0(self):
         self.clock.tick(60)
         self.window.blit(pygame.image.load("background.jpg"), (0, 0))
-        self.window.blit(pygame.font.SysFont('Comic Sans MS', 30).render('Start menu', True, (0, 0, 0)),
-                         (300, 20))
+        self.window.blit(
+            pygame.font.SysFont('Comic Sans MS', 30).render('Start menu', True, (0, 0, 0)),
+            (300, 20))
         self.print_button(self.text)
         for el in self.text:
             if el[4]:
@@ -47,11 +58,23 @@ class Menu:
     def render1(self):
         self.clock.tick(60)
         self.window.blit(pygame.image.load("background.jpg"), (0, 0))
-        self.window.blit(pygame.font.SysFont('Comic Sans MS', 30).render('Settings', True, (0, 0, 0)),
-                         (300, 20))
+        self.window.blit(
+            pygame.font.SysFont('Comic Sans MS', 30).render('Settings', True, (0, 0, 0)),
+            (300, 20))
         self.text_settings[0][2] = "Sound:" + str(self.settings.vol) + "%"
         self.print_button(self.text_settings)
         for el in self.text_settings:
+            if el[4]:
+                self.draw_frame(el[1][0], el[1][1], el[1][2], el[1][3])
+        pygame.display.update()
+
+    def render5(self):
+        self.clock.tick(60)
+        self.window.blit(pygame.image.load("background.jpg"), (0, 0))
+        self.window.blit(pygame.font.SysFont('Comic Sans MS', 30).render('Themes', True, (0, 0, 0)),
+                         (300, 20))
+        self.print_button(self.text_theme)
+        for el in self.text_theme:
             if el[4]:
                 self.draw_frame(el[1][0], el[1][1], el[1][2], el[1][3])
         pygame.display.update()
@@ -67,13 +90,22 @@ class Menu:
                 self.chosen_button(x, y, self.text)
             if self.num_ren == 1:
                 self.chosen_button(x, y, self.text_settings)
+            if self.num_ren == 5:
+                self.chosen_button(x, y, self.text_theme)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.num_ren == 0 and 150 <= x <= 650 and 90 <= y <= 130:
                     self.running = False
                     import src.main as g
                     game = g.GameWindow()
                     game.run()
-                self.into_new_render(x, y)
+                if self.num_ren == 0:
+                    self.into_new_render(x, y, self.text)
+                else:
+                    if self.num_ren == 1:
+                        self.into_new_render(x, y, self.text_settings)
+                    if self.num_ren == 5:
+                        self.into_new_render(x, y, self.text_theme)
+
                 if self.num_ren == 1 and 110 <= x <= 150 and 90 <= y <= 130:
                     self.settings.lower_sound()
                 if self.num_ren == 1 and 660 <= x <= 700 and 90 <= y <= 130:
@@ -82,6 +114,12 @@ class Menu:
                 self.running = False
                 sys.exit()
 
+    def change_theme(self, x, y):
+        for el in self.text_theme:
+            if el[1][0] <= x <= (el[1][0] + el[1][2]) and el[1][1] <= y <= (el[1][3] + el[1][1]):
+                if el[6]:
+                    self.theme = el[5]
+
     def chosen_button(self, x, y, temp):
         for el in temp:
             if el[1][0] <= x <= (el[1][0] + el[1][2]) and el[1][1] <= y <= (el[1][3] + el[1][1]):
@@ -89,19 +127,19 @@ class Menu:
             else:
                 el[4] = False
 
-    def into_new_render(self, x, y):
-        for el in self.text:
-            if self.num_ren == 0 and el[1][0] <= x <= (el[1][0] + el[1][2]) and el[1][1] <= y <= (el[1][3] + el[1][1]):
+    def into_new_render(self, x, y, text):
+        for el in text:
+            if el[1][0] <= x <= (el[1][0] + el[1][2]) and el[1][1] <= y <= (el[1][3] + el[1][1]) and el[5] != -1:
                 self.num_ren = el[5]
-        if self.num_ren != 0 and 0 <= x <= 190 and 0 <= y <= 40:
-            self.num_ren = 0
+        '''if self.num_ren != 0 and 0 <= x <= 190 and 0 <= y <= 40:
+            self.num_ren = 0'''
 
     def draw_frame(self, x, y, lx, ly):
         wildth = 5
-        pygame.draw.line(self.window, (176, 255, 46), (x, y), (x + lx, y), wildth)
-        pygame.draw.line(self.window, (176, 255, 46), (x, y), (x, y + ly), wildth)
-        pygame.draw.line(self.window, (176, 255, 46), (x, y + ly), (x + lx, y + ly), wildth)
-        pygame.draw.line(self.window, (176, 255, 46), (x + lx, y), (x + lx, y + ly), wildth)
+        pygame.draw.line(self.window, self.color[self.theme][1], (x, y), (x + lx, y), wildth)
+        pygame.draw.line(self.window, self.color[self.theme][1], (x, y), (x, y + ly), wildth)
+        pygame.draw.line(self.window, self.color[self.theme][1], (x, y + ly), (x + lx, y + ly), wildth)
+        pygame.draw.line(self.window, self.color[self.theme][1], (x + lx, y), (x + lx, y + ly), wildth)
 
     def run(self):
 
