56750fef72d0bbf2c7323132cd63f53f3a461995
he-2-iiu <chistiakov.chist@gmail.com>
2020-11-25 12:30:15 +0300

    Add add_observer method.

--- src/mode/GameMode.py
+++ src/mode/GameMode.py
@@ -2,6 +2,9 @@ class GameMode:
     def __init__(self):
         self.observers = []
 
+    def add_observer(self, observer):
+        self.observers.append(observer)
+
     def notify_quit_requested(self):
         for observer in self.observers:
             observer.quit_requested()
